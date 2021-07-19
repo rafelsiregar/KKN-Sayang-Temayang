7e16d756653336801a59442e6456bf3de05dc482
Rafel Siregar <michael.rafel@365.ugm.ac.id>
2021-07-19 16:30:12 +0700

    Menambah halaman technopark

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
         window.location.href = '/login.html';
     })
 })
+
+/*var myModal = new bootstrap.Modal(document.getElementById('#profile-popup'), options)
+console.log(myModal)*/
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("google-maps"),mapProp);
+}
