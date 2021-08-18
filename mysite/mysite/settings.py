9f22f1615dedbc23321bdba8f260210ab71f90e3
Rafel Siregar <michael.rafel@365.ugm.ac.id>
2021-08-18 17:48:23 +0700

    Kembali ke SQLite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+#import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#_rc%!=@vb617!oqa*jj_r-g)#j*=7yu13@bgg5v(&c0#3zkdc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '*.heroku.com', '.localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,18 +79,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 #os.path.join(BASE_DIR, 'db.sqlite3')
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd1eblf0v9i6lmv',
-        'USER': 'jckcyhiwzgppbo',
-        'PASSWORD': '5ac89e7550fa7319d9fe5a4438454e9ca814d846c511786414d71748faf6f3be',
-        'HOST': 'ec2-18-235-4-83.compute-1.amazonaws.com',
-        'PORT':  '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
-DATABASE_URL: 'postgres://postgresql-clean-22900'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
