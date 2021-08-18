53cc07f871af98d597439e424f4532b23bdd6ae0
Rafel Siregar <michael.rafel@365.ugm.ac.id>
2021-08-18 13:15:43 +0700

    Menambah database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,9 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE': 500
     }
 }
 
+DATABASE_URL: 'postgres://postgresql-clean-22900'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
